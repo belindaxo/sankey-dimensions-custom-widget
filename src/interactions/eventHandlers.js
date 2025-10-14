f624357e8a6675f298553b3eb63e0f823150c61b
Belinda <bmdebruyn@gmail.com>
2025-10-14 11:42:40 -0600

    fix: revert

--- src/interactions/eventHandlers.js
+++ src/interactions/eventHandlers.js
@@ -81,24 +81,13 @@ export function handlePointClick(event, dataBinding, dimensions, widget) {
         linkedAnalysis.removeFilters();
         widget._selectedPoint.select(false, false);
         widget._selectedPoint = null;
-        const chart = point.series.chart;
-        chart.series.forEach(s => {
-            (s.points || []).forEach(p => p.setState());
-            (s.nodes || []).forEach(n => n.setState());
-        });
     }
 
     if (event.type === 'select') {
         linkedAnalysis.setFilters(selection);
         widget._selectedPoint = point;
-        point.onMouseOver();
     } else if (event.type === 'unselect') {
         linkedAnalysis.removeFilters();
         widget._selectedPoint = null;
-        const chart = point.series.chart;
-        chart.series.forEach(s => {
-            (s.points || []).forEach(p => p.setState());
-            (s.nodes || []).forEach(n => n.setState());
-        });
     }
 }
